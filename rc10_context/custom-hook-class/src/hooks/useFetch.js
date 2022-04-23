7fc74ccc54a3419ef36d5c480fa994181123a9fe
yakupkose3123 <yakupkose3123@gmail.com>
2022-04-23 13:13:59 +0300

    custom hook created, you should use this hook

---
+++ rc10_context/custom-hook-class/src/hooks/useFetch.js
@@ -0,0 +1,29 @@
+import { useState, useEffect} from "react";
+
+function useFetch(url) {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
+
+  useEffect(() => {
+    setLoading(true);
+    fetch(url)
+    .then(response => {
+      if (!response.ok){
+        setError("Something went wrong!");
+        setLoading(false);
+      }else{
+        return response.json()
+      }
+      })
+    .then(data => {
+      setData(data)
+    setLoading(false)});
+  }, [url])
+
+return{data,loading,error};
+  
+}
+
+export default useFetch;
